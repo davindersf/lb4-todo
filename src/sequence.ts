c9a9613ef56e064452fe8ab6d49a20db55a7d090
Davinder Singh <davinder.singh@sourcefuse.com>
2023-02-23 14:19:33 +0530

    feat: Log request start time, referer, user agent, request ip, error time and completion time.

--- src/sequence.ts
+++ src/sequence.ts
@@ -1,3 +1,26 @@
-import {MiddlewareSequence} from '@loopback/rest';
+import {MiddlewareSequence, RequestContext} from '@loopback/rest';
 
-export class MySequence extends MiddlewareSequence {}
+export class MySequence extends MiddlewareSequence {
+  async handle(context: RequestContext) {
+    console.log(`Request arrived at: ${new Date().toLocaleTimeString()}`);
+    const request = context.request;
+    const headers = request.headers;
+
+    console.log(`Referer: ${headers.referer}`);
+    console.log(`User-Agent: ${headers['user-agent']}`);
+    console.log(`IP: ${request.ip}`);
+
+    try {
+      await super.handle(context);
+      console.log(`Response delivered at: ${new Date().toLocaleTimeString()}`);
+    } catch (error) {
+      console.log(`Error occurred at: ${new Date().toLocaleTimeString()}`);
+      throw error;
+    }
+
+    console.log({
+      requestedBaseUrl: context.requestedBaseUrl,
+      basePath: context.basePath,
+    });
+  }
+}
