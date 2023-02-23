8a8c45440dd9682d1083a1ea1172affa1905587f
Davinder Singh <davinder.singh@sourcefuse.com>
2023-02-23 11:04:59 +0530

    feat: Add dotenv to application. Configure a key saying - 'ALLOWED_ORIGIN'

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,6 @@
+import dotenv from 'dotenv';
+dotenv.config();
+
 import {ApplicationConfig, TodoApplication} from './application';
 
 export * from './application';
@@ -20,6 +23,9 @@ if (require.main === module) {
     rest: {
       port: +(process.env.PORT ?? 3000),
       host: process.env.HOST,
+      cors: {
+        origin: process.env.ALLOWED_ORIGIN,
+      },
       // The `gracePeriodForClose` provides a graceful close for http/https
       // servers with keep-alive clients. The default value is `Infinity`
       // (don't force-close). If you want to immediately destroy all sockets
